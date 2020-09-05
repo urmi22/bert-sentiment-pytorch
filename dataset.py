e6148bac41fb750a94f29972cdbf66f4517706ed
urmi22 <debjanimazumder22@example.com>
2020-09-05 20:57:53 +0530

    dataset file created

---
+++ dataset.py
@@ -0,0 +1,13 @@
+import torch
+
+from troch.utils.data import Dataset, DataLoader
+
+
+
+class ImdbReviewDataset(Dataset):
+
+    def __init__(self, datapath, split, max_token_len):
+
+    def __len__(self):
+
+    def __getitem__(self, index):
